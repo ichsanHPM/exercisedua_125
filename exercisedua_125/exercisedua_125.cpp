0e65c3476927cb89ad12b9cd169ae623a70671b9
ichsanHPM <ichsan.helmi.ft22@mail.umy.ac.id>
2023-06-05 10:42:27 +0700

    selesai

--- exercisedua_125/exercisedua_125.cpp
+++ exercisedua_125/exercisedua_125.cpp
@@ -1,12 +1,94 @@
-// exercisedua_125.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class penerbit;
+class pengarang {
+public:
+	string nama;
+	vector<penerbit*> daftar_penerbit;
+	pengarang(string pNama) :nama(pNama) {
+		cout << "Pengarang \"" << nama << "\" ada\n";
+	}
+	~pengarang() {
+		cout << "Pengarang \"" << nama << "\" tidak ada\n";
+	}
+	void tambahPenerbit(penerbit*);
+	void cetakPenerbit();
+};
+
+class penerbit {
+public:
+	string nama;
+	vector<pengarang*> daftar_pengarang;
+
+	penerbit(string pNama) :nama(pNama) {
+		cout << "Penerbit \"" << nama << "\" ada\n";
+	}
+	~penerbit() {
+		cout << "Penerbit \"" << nama << "\" tidak ada\n";
+	}
+
+	void tambahPengarang(pengarang*);
+	void cetakPengarang();
+};
+
+void pengarang::tambahPenerbit(penerbit* pPenerbit) {
+	daftar_penerbit.push_back(pPenerbit);
 }
+void pengarang::cetakPenerbit() {
+	cout << "Daftar Penerbit yang menangani pengarang \"" << this->nama << "\":\n";
+	for (auto& a : daftar_penerbit) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
+void penerbit::tambahPengarang(pengarang* pPengarang) {
+	daftar_pengarang.push_back(pPengarang);
+	pPengarang->tambahPenerbit(this);
+}
+void penerbit::cetakPengarang() {
+	cout << "Daftar Pengarang dari Penerbit \"" << this->nama << "\":\n";
+	for (auto& a : daftar_pengarang) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
+
+int main() {
+	penerbit* varPenerbit1 = new penerbit("Gama.Pres");
+	penerbit* varPenerbit2 = new penerbit("Intan.Pariwara");
+	pengarang* varPengarang1 = new pengarang("Joko");
+	pengarang* varPengarang2 = new pengarang("Lia");
+	pengarang* varPengarang3 = new pengarang("Giga");
+	pengarang* varPengarang4 = new pengarang("Asroni");
+	
+
+	varPenerbit1->tambahPengarang(varPengarang1);
+	varPenerbit1->tambahPengarang(varPengarang2);
+	varPenerbit1->tambahPengarang(varPengarang3);
+	varPenerbit2->tambahPengarang(varPengarang4);
+	varPenerbit2->tambahPengarang(varPengarang3);
+
+	varPenerbit1->cetakPengarang();
+	varPenerbit2->cetakPengarang();
+	varPengarang1->cetakPenerbit();
+	varPengarang2->cetakPenerbit();
+	varPengarang3->cetakPenerbit();
+	varPengarang4->cetakPenerbit();
+
+	delete varPengarang1;
+	delete varPengarang2;
+	delete varPengarang3;
+	delete varPengarang4;
+	delete varPenerbit1;
+	delete varPenerbit2;
+
+	return 0;
+}
+
+
+
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
